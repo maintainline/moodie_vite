ab4978cba0daba691584dfc5bdc328f84c7f428a
maintainline <z.seon.dev@gmail.com>
2025-11-24 16:19:35 +0900

    [docs] 글자수에 따른 코멘트 추가

--- src/components/utils/getLengthComment.js
+++ src/components/utils/getLengthComment.js
@@ -12,5 +12,11 @@ export function getLengthComment(charCount) {
   else if (charCount <= 100) return lengthComments.range_61_100;
   else if (charCount <= 150) return lengthComments.range_101_150;
   else if (charCount <= 200) return lengthComments.range_151_200;
-  else return lengthComments.range_200_plus;
+  else if (charCount <= 300) return lengthComments.range_201_300;
+  else if (charCount <= 500) return lengthComments.range_301_500;
+  else if (charCount <= 700) return lengthComments.range_501_700;
+  else if (charCount <= 1000) return lengthComments.range_701_1000;
+  else if (charCount <= 1500) return lengthComments.range_1001_1500;
+  else if (charCount <= 2000) return lengthComments.range_1501_2000;
+  else return lengthComments.range_2000_plus;
 }
